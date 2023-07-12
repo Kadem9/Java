da5455dcace6ef8f801d3889428bcd61e081c82b
Kadem Garnier <kadem.garnier@gmail.com>
2023-07-12 11:44:39 +0200

    exercise 7

--- javalearning/src/Main.java
+++ javalearning/src/Main.java
@@ -3,13 +3,18 @@ public class Main {
     public static void main(String[] args){
 
         Article article1 = new Article("RFE-89", "Jersey PSG", 59.99);
-
+        ClientMarket client1 = new ClientMarket("Garnier", "Kadem", "5 Rue du Cours", 69007, "Lyon");
 
         printArticle(article1);
+        printClient(client1);
     }
 
     public static void printArticle(Article art) {
         System.out.println("Article : " + art.getRef() + " - " + art.getDesignation() + " - " + art.getPrice() + "€");
+    }
+    public static void printClient(ClientMarket client) {
+        System.out.println("Le client 1 est : " + client.getFirstname() + " " + client.getLastname());
+        System.out.println("Il habite au " + client.getAddress() + " " + client.getCity() + " " + client.getPostal());
     }
 }
 
